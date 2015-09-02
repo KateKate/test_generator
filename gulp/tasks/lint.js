c94fe2f7fbdc42601cde0f5186f60a5a2842769a
Kate Anishkina <anishkina@gmail.com>
2015-09-02 18:48:01 +0300

    Add .eslintrc file

--- gulp/tasks/lint.js
+++ gulp/tasks/lint.js
@@ -14,9 +14,7 @@ gulp.task('lint', () => {
     return gulp.src(['../../src/scripts/**/*.js'])
         // eslint() attaches the lint output to the eslint property
         // of the file object so it can be used by other modules.
-        .pipe(eslint({
-          extends: 'airbnb',
-        }))
+        .pipe(eslint())
         // eslint.format() outputs the lint results to the console.
         // Alternatively use eslint.formatEach() (see Docs).
         .pipe(eslint.format())
